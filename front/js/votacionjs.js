19c73b1e200a56669d4a891e2bd76dc64237761d
Elizabeth Ramirez <eramirezvillamizar@gmail.com>
2019-05-04 21:49:37 -0500

    voto en blanco

--- front/js/votacionjs.js
+++ front/js/votacionjs.js
@@ -10,12 +10,15 @@ function postCargarVotacionPlanchas(result, state){
          var json=JSON.parse(result);
          if(json[0].msg=="exito"){
          	var str ="";
-         	for(var i=1; i < Object.keys(json).length; i++) {
+         	for(var i=2; i < Object.keys(json).length; i++) {
                 var lista = json[i];
                 str += '<div class="col-lg-1 hovereffect"><div class="card"><div class="card-body">';
                 str += '<a href="javascript:preVotar('+lista.numero+')"><button type="button" class="btn btn-primary">'
                 str += lista.numero+'</button></a></div></div></div></div></div>';
             }
+            str += '<div class="col-lg-2 hovereffect"><div class="card"><div class="card-body">';
+            str += '<a href="javascript:preVotar(\'0\')"><button type="button" class="btn btn-primary">'
+            str += 'Voto en Blanco</button></a></div></div></div></div></div>';
             document.getElementById("contenedor").innerHTML+=str;
          }else{
                swal("El representante no se encuentra registrado!\nPor favor registrelo.", {
